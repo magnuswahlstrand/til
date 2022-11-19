b204620d92718d3aa0973cf2c75b3941a490e69f
Magnus <magnus@magnuss-mbp.lan>
2022-11-19 14:16:51 +0100

    fix rss-generator

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import {SITE_DESCRIPTION, SITE_TITLE} from '../config';
+
+
+const postImportResult = import.meta.glob('./blog/**/*.{md,mdx}', { eager: true });
+const posts = Object.values(postImportResult);
 
 export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
+    rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: import.meta.env.SITE,
+        items: posts.map((post) => {
+            return {
+                link: post.url,
+                title: post.frontmatter.title,
+                pubDate: post.frontmatter.datetime,
+            }
+        })
+    });
