1b3735a847ab5bcff3bb1e8fa4f1c0e61ad8ed5b
magnus <magnus.wahlstrand@gmail.com>
2023-08-13 18:52:49 +0200

    improve date format

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,15 +5,18 @@ export interface Props {
 }
 
 export default function Card({title, url, datetime}: Props) {
+    const date = new Date(datetime)
+    let day = date.getDate().toString()
+    let month = date.toLocaleString('default', {month: 'short'});
+    let year = date.getFullYear();
+
+    const padding = day.length == 1 && <span className="text-white">0</span>
+
     return (
         <tr className="text-sm md:text-base">
-            <td className="whitespace-nowrap align-top">
+            <td className="whitespace-nowrap align-top font-mono">
                 <time dateTime={datetime}>
-                    {new Date(datetime).toLocaleDateString([], {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                    })}
+                    {day}{padding} {month} {year}
                 </time>
             </td>
             <td>
